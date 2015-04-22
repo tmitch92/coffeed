8c2f6c73f234885a806e375a146ef5e5c42c1f06
tmitch92 <tyleramitchell92@gmail.com>
2015-04-21 22:25:14 -0300

    Post heroku deploy - index.html added

--- coffeed/wsgi.py
+++ coffeed/wsgi.py
@@ -11,4 +11,4 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeed.settings")
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-application =Cling(get_wsgi_application())
+application = Cling(get_wsgi_application())
